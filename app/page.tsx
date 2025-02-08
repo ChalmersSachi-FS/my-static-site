f15cddd08f7daab970584d3afeeeaea0b4285814
Sachi Chalmers <sachichalmers@Mac.lan>
2025-02-08 12:09:51 -0500

    Folders & Files
    Added all necessary folders & files to my 1.6 exercise

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to My Static Next.js Site</h1>
+      <p>This site is deployed as part of the 1.6 Exercise.</p>
+    </div>
+  );
+}
